35a95cb0fc2de2e8e005a1a16408f1490073b589
Iriemst <iriemdaluz22@gmail.com>
2024-02-28 10:23:12 +0100

    creamos main

---
+++ main.py
@@ -0,0 +1,14 @@
+from ies import alumno
+
+def main():
+    jorge=alumno()
+    jorge.nota= 7
+    jorge.imprimir
+
+    maria=alumno()
+    maria.nota= 3
+    maria.imprimir
+
+    
+if __name__=="__main__":
+    main()
