68c282e902f577cde1cc2fdba9ea5fa84c1c3e97
Iriemst <iriemdaluz22@gmail.com>
2024-02-28 11:11:26 +0100

    hacemos el main

---
+++ main_persona.py
@@ -0,0 +1,15 @@
+from persona import persona
+def main():
+    fabio=persona("Fabio",18)
+    fabio.imprimir()
+    fabio.cumpleanios()
+
+    samu=persona("Samu",54)
+    samu.imprimir()
+    samu.cumpleanios()
+    
+
+if __name__=="__main_persona__":
+    main()
+
+main()
