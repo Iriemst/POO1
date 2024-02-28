7ecd56b71d5a1f1210b946c6c6027ae2e6834e0f
Iriemst <iriemdaluz22@gmail.com>
2024-02-28 10:49:03 +0100

    creado persona.py

---
+++ persona.py
@@ -0,0 +1,12 @@
+class persona:
+    def __init__(self, nombre, anios):
+        self.nombre= nombre
+        self.anios= anios
+
+    def imprimir(self):
+        print(self.nombre, "tiene ",self.anios,"años")
+    
+    def cumpleaños(self):
+        self.anios=self.anios+1
+        print(self.nombre,"cumple un año", "tiene",self.anios)
+        
