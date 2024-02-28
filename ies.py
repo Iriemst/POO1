1b4d8f2f8e7f9aa10df3ec84c7be45662ce5e691
Iriemst <iriemdaluz22@gmail.com>
2024-02-28 10:30:44 +0100

    añadimos def__init__

--- ies.py
+++ ies.py
@@ -1,13 +1,14 @@
 
 class alumno:
+    def __init__(self):
+        self.notas= 0
+        self.nombre=""
+
     def imprimir(self):
-          self.nota= 0
           self.nombre= ""
           print(self.nombre, "obtiene:",self.nota)
 
           def promociona(self):
-            self.nota= 0
-            self.nombre= ""
             if self.nota >= 5:
                 print("Promociona")
             else:
