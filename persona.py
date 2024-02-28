6565183a43457a25eae7fdc8f88d1c42786fe9ca
Iriemst <iriemdaluz22@gmail.com>
2024-02-28 11:11:35 +0100

    correciones

--- persona.py
+++ persona.py
@@ -6,7 +6,7 @@ class persona:
     def imprimir(self):
         print(self.nombre, "tiene ",self.anios,"años")
     
-    def cumpleaños(self):
+    def cumpleanios(self):
         self.anios=self.anios+1
         print(self.nombre,"cumple un año", "tiene",self.anios)
         
