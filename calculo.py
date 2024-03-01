4101ebeab32d6ced14b9fe52be4d07839ab701bc
Iriemst <iriemdaluz22@gmail.com>
2024-03-01 13:48:51 +0100

    escribimoes el codigo de calculo

---
+++ calculo.py
@@ -0,0 +1,20 @@
+class Calculo:
+    def __init__(self, numero1,numero2):
+        self.numero1= numero1
+        self.numero2=numero2
+
+    def suma(self, numero1,numero2):
+        resultado= self.numero1 + self.numero2
+        print(f"Resultado de la suma de {self.numero1} + {self.numero2} = {resultado}")
+
+    def resta(self, numero1,numero2):
+        resultado= self.numero1 - self.numero2
+        print(f"Resultado de la resta de {self.numero1} - {self.numero2} = {resultado}")
+    
+    def multiplicacion(self, numero1,numero2):
+        resultado= self.numero1 * self.numero2
+        print(f"Resultado de la multiplicacion de {self.numero1} * {self.numero2} = {resultado}")
+
+    def division(self, numero1,numero2):
+        resultado= self.numero1 / self.numero2
+        print(f"Resultado de la division de {self.numero1} / {self.numero2} = {resultado}")
