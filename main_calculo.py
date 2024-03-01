0be89a70f9344ee59f36fcbdbf8ef04d6b4c9145
Iriemst <iriemdaluz22@gmail.com>
2024-03-01 13:56:59 +0100

    quitamos main

--- main_calculo.py
+++ main_calculo.py
@@ -1,8 +1,1 @@
 from calculo import Calculo
-
-if __name__=="__main__":
-    calculo=Calculo(5,7)
-    calculo.suma(5,7)
-    calculo.resta(5,7)
-    calculo.multiplicacion(5,7)
-    calculo.division(5,7)
