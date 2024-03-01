c44aa5d151544162c95ab11d222c5020b97bdbf3
Iriemst <iriemdaluz22@gmail.com>
2024-03-01 13:52:50 +0100

    ultimos detalles

--- calculo.py
+++ calculo.py
@@ -5,16 +5,16 @@ class Calculo:
 
     def suma(self, numero1,numero2):
         resultado= self.numero1 + self.numero2
-        print(f"Resultado de la suma de {self.numero1} + {self.numero2} = {resultado}")
+        print(f"Resultado de la suma de {self.numero1} + {self.numero2} es {resultado}")
 
     def resta(self, numero1,numero2):
         resultado= self.numero1 - self.numero2
-        print(f"Resultado de la resta de {self.numero1} - {self.numero2} = {resultado}")
+        print(f"Resultado de la resta de {self.numero1} - {self.numero2} es {resultado}")
     
     def multiplicacion(self, numero1,numero2):
         resultado= self.numero1 * self.numero2
-        print(f"Resultado de la multiplicacion de {self.numero1} * {self.numero2} = {resultado}")
+        print(f"Resultado de la multiplicacion de {self.numero1} * {self.numero2} es {resultado}")
 
     def division(self, numero1,numero2):
         resultado= self.numero1 / self.numero2
-        print(f"Resultado de la division de {self.numero1} / {self.numero2} = {resultado}")
+        print(f"Resultado de la division de {self.numero1} / {self.numero2} es {resultado}")
